b262b1e38867be481406605fa317aadd427bd1ed
mauricioherrera <35494670+mauricioherrera@users.noreply.github.com>
2018-01-21 16:30:12 -0600

    Add files via upload

---
+++ week 1 assignment.py
@@ -0,0 +1,25 @@
+
+# List
+
+myList = [] #empty list
+
+myList.append(1)
+myList.append(2)
+myList.append('b')
+myList.append(56)
+
+print(myList)
+print('The first element in this list is:')
+print(myList[0])
+
+# map
+
+c = {0:'element 1', 1:'element2',2:'element3' }   #empty map
+
+c[0] = 'Value_0'
+c[1] = 'Value_1'
+c[2] = 'Value_2'
+
+print(c)
+print('The value in place of element 1 is:')
+print(c[1])
